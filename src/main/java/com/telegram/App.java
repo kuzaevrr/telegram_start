3962675eb01e57f6359aad01a38f2c74aca4c60c
kuzaevrr <kuzaevrr@mail.ru>
2021-05-24 23:15:16 +0500

    Initial commit

---
+++ src/main/java/com/telegram/App.java
@@ -0,0 +1,24 @@
+package com.telegram;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(Bot.getBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+}
